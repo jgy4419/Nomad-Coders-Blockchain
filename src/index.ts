78875886c86e75dc16fa45f38d66bc8c85cdb3eb
jgy4419 <jgy_98@naver.com>
2022-06-23 00:40:30 +0900

    done :)

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,39 @@ class Block implements BlockShape{
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain{
+    private blocks: Block[]
+    constructor(){
+        this.blocks = [];;
+    }
+    private getPrevHash(){
+        // 만약 blocks의 길이인 this.blocks.length가 0이면
+        if(this.blocks.length === 0) return "" // 첫 번재 hash가 없으므로 ""리턴
+        return this.blocks[this.blocks.length - 1].hash // 만약 그게 아니면 마지막 블럭의 해쉬값을 리턴
+    }
+    // 새로운 블록을 추가할 때는, 블록에 저장하고 싶은 데이터 보내주기.
+    public addBlock(data:string){
+        const newBlock = new Block(
+            this.getPrevHash(), 
+            this.blocks.length + 1, 
+            data
+        );
+        this.blocks.push(newBlock); 
+    }
+    // 블록에 접근할 수 있는 함수
+    public getBlocks(){
+        // 아예 새로운 배열을 return 해줌으로 가짜 데이터 들어오는 문제 해결.(해킹문제)
+        return [...this.blocks];
+    }
+}
+
+// 새로운 블록채인 생성.
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Four one");
+
+console.log(blockchain.getBlocks());
